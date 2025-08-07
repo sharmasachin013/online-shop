e30ae30108e44d681b683ea9fc2bbbeb09445efe
Sachin Sharma <sharma.sachin013@gmail.com>
2025-08-07 07:22:10 +0000

    config_sync_directory settings update

--- drupal/web/sites/default/settings.php
+++ drupal/web/sites/default/settings.php
@@ -259,6 +259,8 @@ $databases = [];
  * its location.
  */
 # $settings['config_sync_directory'] = '/directory/outside/webroot';
+$settings['config_sync_directory'] = '../config/sync';
+
 
 /**
  * Settings:
